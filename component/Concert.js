a0cfe922546266a87b15ad37aacde946a13dffa3
Mohammad Runna <mohammadrunnasheikh@gmail.com>
2025-08-28 11:04:50 +0530

    gdrive

--- component/Concert.js
+++ component/Concert.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
@@ -8,15 +9,18 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-const images = [
-  "/jha10.jpg",
-  "/jha7.jpg",
-  "/jha8.jpg",
-  "/jha12.jpg",
-  "/jha6.jpg",
-];
-
 export default function Concert() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const res = await fetch("/api/concert");
+      const data = await res.json();
+      setImages(data.images || []);
+    };
+    fetchImages();
+  }, []);
+
   return (
     <motion.section
       initial={{ y: 100, opacity: 0 }}
@@ -32,22 +36,22 @@ export default function Concert() {
       <Swiper
         modules={[Navigation, Pagination]}
         spaceBetween={20}
-        slidesPerView={1} // default → mobile = 1 slide
+        slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
         breakpoints={{
-          640: { slidesPerView: 1 }, // phones
-          768: { slidesPerView: 2 }, // tablets
-          1024: { slidesPerView: 3 }, // desktops
+          640: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
         }}
         loop={true}
       >
-        {images.map((src, index) => (
+        {images.map((img, index) => (
           <SwiperSlide key={index}>
             <div className="flex justify-center">
               <Image
-                src={src}
-                alt={`Concert ${index + 1}`}
+                src={img.src}
+                alt={img.alt}
                 width={1200}
                 height={800}
                 className="rounded-lg object-cover w-full h-[400px]"
